fc5e424e2f870a30b8e4ac303db3230312c2d259
dwisabrina23 <dwisabrina10@gmail.com>
2021-11-13 01:36:04 +0800

    serach testing

--- src/pages/Search.test.jsx
+++ src/pages/Search.test.jsx
@@ -1,49 +1,49 @@
-import React from 'react';
-import axios from 'axios';
-import { render, screen, act } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
- 
-import Search from './Search';
- 
-jest.mock('axios');
- 
-describe('Search', () => {
-  test('fetches stories from an API and displays them', async () => {
+import React from "react";
+import axios from "axios";
+import { render, screen, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Search from "./Search";
+
+jest.mock("axios");
+
+describe("Search", () => {
+  test("fetches stories from an API and displays them", async () => {
     const stories = [
-      { objectID: '1', title: 'Hello' },
-      { objectID: '2', title: 'React' },
+      { objectID: "1", title: "Test1" },
+      { objectID: "2", title: "Test2" },
+      { objectID: "3", title: "Test3" },
+
     ];
- 
+
     axios.get.mockImplementationOnce(() =>
       Promise.resolve({ data: { hits: stories } })
     );
- 
+
     render(<Search />);
- 
+
     await act(async () => {
-      await userEvent.click(screen.getByRole('button'));
+      await userEvent.click(screen.getByRole("button"));
     });
-    
- 
-    const items = await screen.findAllByRole('listitem');
- 
-    expect(items).toHaveLength(2);
-  });
- 
-  test('fetches stories from an API and fails', async () => {
-    axios.get.mockImplementationOnce(() =>
-      Promise.reject(new Error('Error Jaringan'))
-    );
- 
-    render(<Search />);
- 
-    await act(async() => {
-      await userEvent.click(screen.getByRole('button'));
-    })
- 
-    const message = await screen.findByText(/Ada yang error/);
- 
-    expect(message).toBeInTheDocument();
+
+    const items = await screen.findAllByRole("listitem");
+
+    expect(items).toHaveLength(3);
   });
 
+  test("fetches stories from an API and fails", async () => {
+    axios.get.mockImplementationOnce(() =>
+      Promise.reject(new Error("Error Jaringan"))
+    );
+
+    render(<Search />);
+
+    await act(async () => {
+      await userEvent.click(screen.getByRole("button"));
+    });
+
+    const message = await screen.findByText(/Ada yang error/);
+
+    expect(message).toBeInTheDocument();
+  });
 });
