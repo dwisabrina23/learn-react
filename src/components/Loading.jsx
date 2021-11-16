d0d246a100385c35531c21d6816d7b55e9836a68
dwisabrina23 <dwisabrina10@gmail.com>
2021-11-16 19:12:59 +0800

    react-hasura exercise

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Loading(props) {
+    return (
+        <div>
+            <p className="text-center">Loading..</p>
+            {/* <img className="d-flex justify-content-center align-item-center" src="../assets/loading.svg" alt="" /> */}
+        </div>
+    );
+}
+
+export default Loading;
